881134acc88a49a047d7e91e0348e0ed3887f40d
AhmedA <alkhatib_ahmed1995@hotmail.com>
2022-09-26 09:04:34 -0400

    refactored code to chain together rest methods through app route method

--- app.js
+++ app.js
@@ -23,7 +23,9 @@ const articleSchema = {
 
 const Article = mongoose.model("Article", articleSchema);
 
-app.get("/articles", function(req, res){
+app.route("/articles")
+
+.get(function(req, res){
     Article.find(function(err, foundArticles){
         if (!err) {
             res.send(foundArticles);
@@ -32,9 +34,9 @@ app.get("/articles", function(req, res){
         }
         
     }); 
-});
+})
 
-app.post("/articles", function(req, res){
+.post(function(req, res){
 
     const newArticle = new Article({
         title: req.body.title,
@@ -48,9 +50,9 @@ app.post("/articles", function(req, res){
             res.send(err);
         }
     });
-});
+})
 
-app.delete("/articles", function(req, res){
+.delete(function(req, res){
     Article.deleteMany(function(err){
         if (!err){
             res.send("Successfully deleted all articles.");
@@ -63,3 +65,27 @@ app.delete("/articles", function(req, res){
 app.listen(3000, function() {
     console.log("Server started on port 3000");
 });
+
+/*
+{
+        "_id": "632db2301bd1ae430ee42717",
+        "title": "REST",
+        "content": "REST is short for REpresentational State Transfer. It's an architectural style for designing APIs."
+    },
+    {
+        "_id": "632db2b61bd1ae430ee42718",
+        "title": "Bootstrap",
+        "content": "This is a framework developed by Twitter that contains pre-made front-end templplates for websites"
+    },
+    {
+        "_id": "632db2f11bd1ae430ee42719",
+        "title": "DOM",
+        "content": "The Document Object Model is like an API for interacting with our HTML"
+    },
+    {
+        "_id": "63319f41ca1b36e424696b93",
+        "title": "Jack Bauer",
+        "content": "Jack Bauer once stepping into quicksand. The quicksand couldn't escape and nearly drowned.",
+        "__v": 0
+    }
+*/
